f2b980cc0a6f2646f1f4129eb6e3a07ed8e97603
Ssekiwala-Colline <ssekiwal8@gmail.com>
2022-11-30 14:06:39 +0000

    Added 8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "python is an interpreted, interactive, objective-oriented programming\
+       language that combines remarkable power with very clear syntax"
+       str = str[39:66] + str[106:112] + str [:6]
+       print(str)
+
