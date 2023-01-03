ca8b9b0306b8dfb5bf9e8c1e1bcc73cae2e7ec89
Ssekiwala-Colline <ssekiwal8@gmail.com>
2023-01-03 19:30:18 +0000

    Added 1-rectangle.py

---
+++ 0x08-python-more_classes/tests/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+rect1 = Rectangle(4, 5)
+print(rect1.width)  # Output: 4
+print(rect1.height)  # Output: 5
+
+rect2 = Rectangle()
+print(rect2.width)  # Output: 0
+print(rect2.height)  # Output: 0
